73d7764d3ebf03ea1e13292de09d34a83c425b80
Michael Ko. Gajda <draget@speciesm.net>
2020-10-05 18:06:13 +0200

    Fix equals

--- main.spec.ts
+++ main.spec.ts
@@ -1,9 +1,9 @@
 import {add, mul} from "./main";
 
 it('test add', () => {
-    expect(add(2,3)).toBe(5)
+    expect(add(2,3)).toEqual(5)
 })
 
 it('test mul', () => {
-    expect(mul(2,3)).toBe(6)
+    expect(mul(2,3)).toEqual(6)
 })
