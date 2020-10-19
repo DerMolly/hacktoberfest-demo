483740f9f6c341922f79476f2abd2f0773be4973
Michael G <draget@speciesm.net>
2020-10-19 17:57:16 +0200

    Add substration (#8)
    Fixes #7

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ export const add = (a: number, b: number): number => {
 	return a + b;
 }
 
+// Sub function
+export const sub = (a: number, b: number): number => {
+	return a - b;
+}
+
 export const mul = (a: number, b: number): number => {
 	return a * b;
 }
